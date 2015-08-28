c36245a77b21f5921972d8cc2819369f5e5e4ee3
Ryan Walsh <ryan@thehatrack.net>
2015-08-27 21:45:34 -0600

    Complete

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+var color = 'white';
+var colors = 'red blue green yellow white'
+
+$('.box').click(function() {
+	$(this).addClass(color)
+})
+
+$('#reset').click(function() {
+	$('.box').removeClass(colors)
+})
+
+$('#red').click(function(event) {
+	color = 'red';
+})
+
+$('#green').click(function(event) {
+	color = 'green';
+})
+$('#blue').click(function(event) {
+	color = 'blue';
+})
+$('#yellow').click(function(event) {
+	color = 'yellow';
+})
+$('#white').click(function(event) {
+	color = 'white';
+})
+
+
+
+
+
+
+});
